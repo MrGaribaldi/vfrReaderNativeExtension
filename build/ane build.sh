0ab662c7236c4b8c134bd29bc5fae94bdfe5cf33
yizhakitzchaki <tzachikk@gmail.com>
2016-08-19 12:48:59 +0200

    updated to use latest flex and air, recompiled to .ane

--- build/ane build.sh
+++ build/ane build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-adt -package -target ane VfrPdfReader.ane ./extension.xml -swc VfrPdfReader.swc -platform iPhone-ARM -C ./ios . -platformoptions ios-platformoptions.xml -platform iPhone-x86 -C ./ios_simulator . -platformoptions ios-platformoptions.xml -platform default library.swf
+"/Applications/Apache Flex/4.15_and_air/bin/adt" -package -target ane VfrPdfReader.ane ./extension.xml -swc VfrPdfReader.swc -platform iPhone-ARM -C ./ios . -platformoptions ios-platformoptions.xml -platform iPhone-x86 -C ./ios_simulator . -platformoptions ios-platformoptions.xml -platform default library.swf
